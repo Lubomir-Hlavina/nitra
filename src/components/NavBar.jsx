f27b17bca85b0fa0be0c8292967a156f591116de
Lubomir-Hlavina <lubo.hlavina@gmail.com>
2025-05-08 16:38:45 +0200

    navbar redisigned

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -16,12 +16,18 @@ import {
   useMediaQuery,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
+import HomeIcon from '@mui/icons-material/Home';
+import MenuBookIcon from '@mui/icons-material/MenuBook';
+import EventIcon from '@mui/icons-material/Event';
+import LocalDiningIcon from '@mui/icons-material/LocalDining';
+import PeopleIcon from '@mui/icons-material/People';
 import { Link } from 'react-router-dom';
 
 const NavBar = () => {
   const [mobileOpen, setMobileOpen] = useState(false);
   const [anchorEl, setAnchorEl] = useState(null);
   const isMenuOpen = Boolean(anchorEl);
+  const [activeMenu, setActiveMenu] = useState('');
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down('md'));
@@ -30,34 +36,42 @@ const NavBar = () => {
     setMobileOpen(!mobileOpen);
   };
 
-  const handleMenuClick = (event) => {
+  const handleMenuClick = (event, menuType) => {
     setAnchorEl(event.currentTarget);
+    setActiveMenu(menuType);
   };
 
   const handleMenuClose = () => {
     setAnchorEl(null);
+    setActiveMenu('');
   };
 
   const drawer = (
     <Box sx={{ width: 250 }} onClick={handleDrawerToggle}>
       <List>
         <ListItem button component={Link} to="/">
+          <HomeIcon sx={{ mr: 1 }} />
           <ListItemText primary="Domov" />
         </ListItem>
+        <ListItem button component={Link} to="/novinky">
+          <EventIcon sx={{ mr: 1 }} />
+          <ListItemText primary="Novinky" />
+        </ListItem>
         <ListItem button component={Link} to="/menu">
+          <MenuBookIcon sx={{ mr: 1 }} />
           <ListItemText primary="Menu" />
         </ListItem>
         <ListItem button component={Link} to="/udalosti">
+          <EventIcon sx={{ mr: 1 }} />
           <ListItemText primary="Udalosti" />
         </ListItem>
-        <ListItem button component={Link} to="/podniky/gastro">
-          <ListItemText primary="Podniky – Gastro" />
+        <ListItem button component={Link} to="/podniky">
+          <LocalDiningIcon sx={{ mr: 1 }} />
+          <ListItemText primary="Podniky" />
         </ListItem>
-        <ListItem button component={Link} to="/podniky/sluzby">
-          <ListItemText primary="Podniky – Služby" />
-        </ListItem>
-        <ListItem button component={Link} to="/podniky/remesla">
-          <ListItemText primary="Podniky – Remeslá" />
+        <ListItem button component={Link} to="/sluzby">
+          <PeopleIcon sx={{ mr: 1 }} />
+          <ListItemText primary="Služby" />
         </ListItem>
       </List>
     </Box>
@@ -84,60 +98,140 @@ const NavBar = () => {
             </IconButton>
           ) : (
             <Box sx={{ display: 'flex', gap: 2 }}>
-              <Button color="inherit" component={Link} to="/">
+              <Button
+                color="inherit"
+                component={Link}
+                to="/"
+                startIcon={<HomeIcon />}
+              >
                 Domov
               </Button>
-              <Button color="inherit" component={Link} to="/novinky">
+              <Button
+                color="inherit"
+                component={Link}
+                to="/novinky"
+                startIcon={<EventIcon />}
+              >
                 Novinky
               </Button>
-              <Button color="inherit" component={Link} to="/menu">
+              <Button
+                color="inherit"
+                component={Link}
+                to="/menu"
+                startIcon={<MenuBookIcon />}
+              >
                 Menu
               </Button>
-              <Button color="inherit" component={Link} to="/udalosti">
+              <Button
+                color="inherit"
+                component={Link}
+                to="/udalosti"
+                startIcon={<EventIcon />}
+              >
                 Udalosti
               </Button>
               <Button
                 color="inherit"
-                onClick={handleMenuClick}
+                onClick={(e) => handleMenuClick(e, 'podniky')}
                 aria-controls={isMenuOpen ? 'podniky-menu' : undefined}
                 aria-haspopup="true"
                 aria-expanded={isMenuOpen ? 'true' : undefined}
+                startIcon={<LocalDiningIcon />}
               >
                 Podniky
               </Button>
               <Menu
                 id="podniky-menu"
                 anchorEl={anchorEl}
-                open={isMenuOpen}
+                open={activeMenu === 'podniky' && isMenuOpen}
                 onClose={handleMenuClose}
               >
                 <MenuItem
                   component={Link}
-                  to="/podniky/gastro"
+                  to="/podniky/restauracie"
                   onClick={handleMenuClose}
                 >
-                  Gastro
+                  Reštaurácie
                 </MenuItem>
                 <MenuItem
                   component={Link}
-                  to="/podniky/sluzby"
+                  to="/podniky/kaviarne"
                   onClick={handleMenuClose}
                 >
-                  Služby
+                  Kaviarne
                 </MenuItem>
                 <MenuItem
                   component={Link}
-                  to="/podniky/remesla"
+                  to="/podniky/fastfoody"
+                  onClick={handleMenuClose}
+                >
+                  Fastfoody
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/podniky/puby"
+                  onClick={handleMenuClose}
+                >
+                  Puby
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/podniky/cukrarne"
+                  onClick={handleMenuClose}
+                >
+                  Cukrárne
+                </MenuItem>
+              </Menu>
+              <Button
+                color="inherit"
+                onClick={(e) => handleMenuClick(e, 'sluzby')}
+                aria-controls={isMenuOpen ? 'sluzby-menu' : undefined}
+                aria-haspopup="true"
+                aria-expanded={isMenuOpen ? 'true' : undefined}
+                startIcon={<PeopleIcon />}
+              >
+                Služby
+              </Button>
+              <Menu
+                id="sluzby-menu"
+                anchorEl={anchorEl}
+                open={activeMenu === 'sluzby' && isMenuOpen}
+                onClose={handleMenuClose}
+              >
+                <MenuItem
+                  component={Link}
+                  to="/sluzby/ubytovanie"
+                  onClick={handleMenuClose}
+                >
+                  Ubytovanie
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/sluzby/remesla"
                   onClick={handleMenuClose}
                 >
                   Remeslá
                 </MenuItem>
                 <MenuItem
                   component={Link}
-                  to="/podniky/hotely"
+                  to="/sluzby/osobna-starostlivost"
                   onClick={handleMenuClose}
                 >
-                  Hotely
+                  Osobná starostlivosť
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/sluzby/fitness"
+                  onClick={handleMenuClose}
+                >
+                  Fitness
+                </MenuItem>
+                <MenuItem
+                  component={Link}
+                  to="/sluzby/zdravie"
+                  onClick={handleMenuClose}
+                >
+                  Zdravie
                 </MenuItem>
               </Menu>
             </Box>
--- src/routes/AppRouter.jsx
+++ src/routes/AppRouter.jsx
@@ -1,17 +1,39 @@
-import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layout/MainLayout";
-import Home from "../pages/Home";
-import BusinessesPage from "../features/businesses/BusinessesPage";
-import EventsPage from "../features/events/EventsPage"; 
+import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../layout/MainLayout';
+import Home from '../pages/Home';
+import BusinessesPage from '../features/businesses/BusinessesPage';
+import EventsPage from '../features/events/EventsPage';
+import HotelsPage from '../features/services/HotelsPage';
+import CraftsPage from '../features/services/CraftsPage';
+import SelfCarePage from '../features/services/SelfCarePage';
+import FitnessPage from '../features/services/FitnessPage';
+import HealthPage from '../features/services/HealthPage';
+import ErrorPage from '../components/ErrorPage';
+import CafesPage from '../features/businesses/CafesPage';
+import RestaurantsPage from '../features/businesses/RestaurantsPage';
+import FastFoodPage from '../features/businesses/FastFoodPage';
+import PubsPage from '../features/businesses/PubsPage';
+import ConfectioneriesPage from '../features/businesses/ConfectioneriesPage';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <MainLayout />,
+    errorElement: <ErrorPage />,
     children: [
-      { path: "", element: <Home /> },
-      { path: "podniky/:category", element: <BusinessesPage /> }, // dynamická kategória
-      { path: "udalosti", element: <EventsPage /> }, // premenované z "eventy" na "udalosti"
+      { path: '', element: <Home /> },
+
+      { path: 'podniky/restauracie', element: <RestaurantsPage /> },
+      { path: 'podniky/kaviarne', element: <CafesPage /> },
+      { path: 'podniky/fastfoody', element: <FastFoodPage /> },
+      { path: 'podniky/puby', element: <PubsPage /> },
+      { path: 'podniky/cukrarne', element: <ConfectioneriesPage /> },
+      { path: 'udalosti', element: <EventsPage /> },
+      { path: 'sluzby/ubytovanie', element: <HotelsPage /> },
+      { path: 'sluzby/remesla', element: <CraftsPage /> },
+      { path: 'sluzby/osobna-starostlivost', element: <SelfCarePage /> },
+      { path: 'sluzby/fitness', element: <FitnessPage /> },
+      { path: 'sluzby/zdravie', element: <HealthPage /> },
     ],
   },
 ]);
